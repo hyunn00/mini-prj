676a62357ab27aa0fd50fcdc7f9daaf00dfcd2bd
Ko <eorn21301@gmail.com>
2022-02-16 16:50:29 +0900

    qui basic 2, quiz 2

---
+++ GUI Prj/gui_basic/8_combobox.py
@@ -0,0 +1,25 @@
+from tkinter import *
+import tkinter.ttk as ttk
+
+root = Tk()
+root.title('Nado GUI')
+root.geometry("640x480")
+
+values = [str(i) + "일" for i in range(1, 32)]
+combobox = ttk.Combobox(root, height = 5, value = values)
+combobox.pack()
+combobox.set("카드 결제일") # 최초 목록 제목 설정
+
+readonly_combobox = ttk.Combobox(root, height = 10, value = values, state = "readonly")
+readonly_combobox.current(0) # 0 번째 인덱스 값 선택
+readonly_combobox.pack()
+
+def btncmd() :
+    print(combobox.get()) # 선택된 값 표시
+    print(readonly_combobox.get())
+    
+btn = Button(root, text = "선택", command = btncmd)
+btn.pack()
+
+root.mainloop()
+
--- GUI Prj/gui_quiz/quiz.py
+++ GUI Prj/gui_quiz/quiz.py
@@ -1,44 +1,46 @@
 from tkinter import *
-
-def fileOpen() :
-    file = open('GUI Prj\gui_quiz\mynote.txt', 'r', encoding='utf-8')
-    data = file.read()
-    txt.insert(END, data)
-
-def fileSave() :
-    pass
-
-def exit() :
-    root.quit()
-    root.destroy()
+import os
 
 root = Tk()
 root.title("제목 없음 - window 메모장")
 root.geometry("800x600")
 
+filename = "mynote.txt"
+
+def fileOpen() :
+    if os.path.isfile(filename) : # 파일이 있으면 true, 없으면 false
+        with open(filename, 'r', encoding = "utf-8") as file :
+            txt.delete("1.0", END) # 텍스트 위젯 본문 삭제
+            txt.insert(END, file.read()) # 파일 내용을 본문에 입력
+
+def fileSave() :
+     with open(filename, 'w', encoding = "utf-8") as file :
+            file.write(txt.get("1.0", END)) # 모든 내용을 가져와서 저장
+
 # 메뉴 바
 mainMenu = Menu(root)
-root.config(menu = mainMenu)
 
 fileMenu = Menu(mainMenu, tearoff=0)
 fileMenu.add_command(label = "열기", command = fileOpen)
 fileMenu.add_command(label = "저장", command = fileSave)
-fileMenu.add_command(label = "끝내기", command = exit)
-
+fileMenu.add_separator()
+fileMenu.add_command(label = "끝내기", command = root.quit)
 mainMenu.add_cascade(label = "파일", menu=fileMenu)
+
+# 편집, 서식, 보기, 도움말
 mainMenu.add_cascade(label = "편집")
 mainMenu.add_cascade(label = "서식")
 mainMenu.add_cascade(label = "보기")
 mainMenu.add_cascade(label = "도움말")
 
-# frame
-frame = Frame(root)
-frame.pack(fill = "both", expand = True)
+root.config(menu = mainMenu)
 
-scroll = Scrollbar(frame)
+# 스크롤 바
+scroll = Scrollbar(root)
 scroll.pack(side = "right", fill = "y")
 
-txt = Text(frame, yscrollcommand = scroll.set)
+# 본문 영역
+txt = Text(root, yscrollcommand = scroll.set)
 txt.pack(side = "left", fill = "both", expand = True)
 
 scroll.config(command = txt.yview)
